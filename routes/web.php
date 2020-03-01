43e02f04fe76a751b98c94844c824a44bc3363b7
Ryan France <r9france@outlook.com>
2020-03-01 10:58:26 +0000

    Laravel 6 warmup, ep 7 laracasts

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts/closure/{post}', function($post) {
+    $posts = [
+        'first-post'    => 'This be the first one',
+        'second-post'   => 'This be the second one'
+    ];
+
+    abort_unless(array_key_exists($post, $posts), 404, 'Post not found');
+
+    return $posts[$post];
+});
